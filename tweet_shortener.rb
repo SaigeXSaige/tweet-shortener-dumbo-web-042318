d53e59c8d5ca58f618499d2d0355a9af5000b47e
Saige Leslie <saigeleslie@gmail.com>
2018-04-12 22:02:19 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,7 +17,8 @@ def word_substituter(tweets)
   short_tweets = []
   tweets.split.collect do |word|
     if dictionary.keys.include?(word)
-      dictionary.values.each do |short_word|
+      short_tweet = dictionary.values
+      short_word.each do |short_word|
         word.replace "#{short_word}"
       end
     end
