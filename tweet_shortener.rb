4abf6fd6b399dbc000587fb98188305661330e16
Saige Leslie <saigeleslie@gmail.com>
2018-04-12 21:45:42 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+require 'pry'
+
+def dictionary
+  dictionary = 
+  { "hello" => 'hi',
+    "to, two, too" => '2', 
+    "for, four" => '4',
+    "be" => 'b',
+    "you" => 'u',
+    "at" => '@',
+    "and" => '&',
+  }
+  
+end
+
+def word_substituter(tweets)
+  tweets.split.collect do |word|
+    if dictionary.keys.include?(word)
+      puts word
+    end
+  end
+  
+  binding.pry
+end
